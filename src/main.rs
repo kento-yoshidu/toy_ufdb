d39c29ea147762197c14e307e68b92983f2e0e14
kento-yoshidu <kento_yoshidu@yahoo.co.jp>
2026-07-08 21:31:53 +0900

    ⚡ Update

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+use std::io::{self, BufRead};
+
 use clap::{Parser, Subcommand};
+use toy_ufdb::Ufdb;
 
 #[derive(Parser)]
 struct Cli {
@@ -15,17 +18,29 @@ enum Commands {
 }
 
 fn main() {
-    let cli = Cli::parse();
+    let mut ufdb = Ufdb::new();
 
-    match cli.command {
-        Commands::Hello => {
-            println!("Hello World");
-        },
-        Commands::Insert { key } => {
-            println!("key = {key}");
-        },
-        Commands::Exit => {
-            println!("bye.");
-        },
+    for line in io::stdin().lock().lines() {
+        let line = line.unwrap();
+
+        let mut tokens = vec!["repl"];
+        tokens.extend(line.split_whitespace());
+
+        match Cli::try_parse_from(tokens) {
+            Ok(cli) => match cli.command {
+                Commands::Hello => {
+                    println!("Hello World");
+                },
+                Commands::Insert { key } => {
+                    let inserted = ufdb.make_set(&key);
+                    println!("{inserted}");
+                },
+                Commands::Exit => {
+                    println!("bye.");
+                    break;
+                },
+            },
+            Err(e) => eprintln!("{e}"),
+        }
     }
 }
